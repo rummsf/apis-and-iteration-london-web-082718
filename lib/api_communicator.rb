91964d94a3a33e1b7033660deb88523fe625bea1
Irum <irum@Irums-MacBook-Air.local>
2018-08-29 17:03:15 +0100

    completed

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,37 +1,27 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-
 def get_character_movies_from_api(character)
-  #make the web request
+  film_arr = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  response_hash["results"].each do |char|
+    if char["name"].downcase == character
+      char["films"].each do |link|
+        response_filminfo = RestClient.get(link)
+        film_arr << response_film = JSON.parse(response_filminfo)
+      end
+    end
+  end
+  film_arr
 end
-
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def print_movies(films_array)
+    p "This character stars in:"
+    films_array.each do |detail|
+      p "#{detail["title"]}, directed by #{detail["director"]}."
+  end
 end
-
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  p "Hello! Welcome to Star Wars world."
 end
-
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  p "please enter a character"
+  user_input = gets.chomp
+  return user_input.downcase
 end
